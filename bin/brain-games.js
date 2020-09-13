44b2e7196d551e52fe1c03d93891aef962b9e5d1
nichegosebe <t788831@gmail.com>
2020-09-13 20:20:33 +0300

    github actions test

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin / env node
 
-import { greeting } from '../src/cli';
+import { greeting } from '../src/cli.js';
 
 greeting();
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-debugger, no-console */
 import readlineSync from 'readline-sync';
 
-const getName = () => readlineSync.question('May I have your name? ');
+const getName = () => readlineSync.question('May I have your name?');
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
   console.log(`Hello, ${getName()}!`);
 };
 
-export default greeting;
+export { greeting };
