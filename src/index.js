21266fd17cc5e7830b07305ec211e9e0fe8e852d
nichegosebe <t788831@gmail.com>
2020-09-20 13:15:28 +0300

    logics files moved to "games" folder

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const gameRound = (generateQuestion) => {
   if (usersAnswer === correctAnswer) {
     say('Correct!');
   } else {
-    say(`"${usersAnswer}" is wrong answer ;(. Correct answer was ${correctAnswer}!`);
+    say(`"${usersAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}"!`);
   }
 
   return usersAnswer === correctAnswer;
